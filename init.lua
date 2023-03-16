4243965adfd78c453fb208015eda83e2bc9d43b8
Travis Abram <tabram@starbucks.com>
2023-03-15 22:35:56 -0700

    feat: hide duplicate bar at bottom

--- init.lua
+++ init.lua
@@ -234,6 +234,9 @@ vim.o.completeopt = 'menuone,noselect'
 -- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
 
+-- Hide duplicate insert/visual status
+vim.o.showmode = false
+
 -- [[ Basic Keymaps ]]
 
 -- Keymaps for better default experience
