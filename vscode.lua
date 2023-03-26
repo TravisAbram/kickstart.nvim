49e53d9c4f99b7c125d2c968ebadd68a7980b45e
Travis Abram <tabram@starbucks.com>
2023-03-26 09:49:23 -0700

    feat: add vscode.lua

---
+++ vscode.lua
@@ -0,0 +1,21 @@
+# VSCode neovim config
+
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+vim.opt.rtp:prepend(lazypath)
+
+-- NOTE: Here is where you install your plugins.
+--  You can configure plugins using the `config` key.
+--
+--  You can also configure plugins after the setup call,
+--    as they will be available in your neovim runtime.
+require('lazy').setup(
+  {
+    "ggandor/leap.nvim",
+    config = function()
+      require('leap').add_default_mappings()
+    end,
+  },
+  { 'numToStr/Comment.nvim', opts = {} }
+)
+
+vim.o.clipboard = 'unnamedplus'
